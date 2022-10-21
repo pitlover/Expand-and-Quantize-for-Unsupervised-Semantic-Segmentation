39bb7e3cf1728a20b1e1db99eadce13eb01b4629
jiyoungkim <pitlover@naver.com>
2022-10-21 20:48:51 +0900

    [FIX] 10/21 NEW_PQ_baseline_l2_kmeansinitial_4096_top1

--- model/dino_new_vq.py
+++ model/dino_new_vq.py
@@ -2,6 +2,7 @@ from typing import Dict, Tuple, List, Optional
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import random
 
 from model.dino import DinoFeaturizer
 from model.blocks.resnet import EncResBlock, DecResBlock
@@ -9,6 +10,25 @@ from model.blocks.resnet import EncResBlock, DecResBlock
 from utils.dist_utils import all_reduce_tensor
 import numpy as np
 from sklearn.cluster import KMeans
+import faiss
+
+
+@torch.no_grad()
+def get_histogram_count(count: torch.Tensor, prefix: str = "") -> Dict:
+    prob = count.float() / (count.sum() + 1)  # (K,)
+    num_codebook = len(prob)
+
+    prob, _ = torch.sort(prob, dim=0, descending=True)  # (K,)
+    c_sum = torch.cumsum(prob, dim=0)  # (K,)
+    output = {f"{prefix}-p10": None, f"{prefix}-p50": None, f"{prefix}-p90": None}
+    for i in range(len(c_sum)):
+        if (c_sum[i] >= 0.9) and (output[f"{prefix}-p90"] is None):
+            output[f"{prefix}-p90"] = i / num_codebook
+        if (c_sum[i] >= 0.5) and (output[f"{prefix}-p50"] is None):
+            output[f"{prefix}-p50"] = i / num_codebook
+        if (c_sum[i] >= 0.1) and (output[f"{prefix}-p10"] is None):
+            output[f"{prefix}-p10"] = i / num_codebook
+    return output
 
 
 class DINONewVq(nn.Module):
@@ -37,10 +57,12 @@ class DINONewVq(nn.Module):
         self.vq_type = cfg["vq"]["vq_type"]
         self.normalize = cfg["vq"].get("normalize", "none")
         self.use_weighted_sum = cfg["vq"].get("use_weighted_sum", False)
+        self.use_restart = cfg["vq"].get("use_restart", False)
         self.need_initialized = cfg["vq"].get("need_initialized", False)
 
         vq_kwargs = dict(beta=self.beta,
                          normalize=self.normalize,
+                         use_restart=self.use_restart,
                          use_weighted_sum=self.use_weighted_sum,
                          need_initialized=self.need_initialized)
 
@@ -117,6 +139,7 @@ class Codebook(nn.Module):
                  latent_dim: int,
                  beta=0.25,
                  normalize: str = "none",
+                 use_restart: bool = False,
                  use_weighted_sum: bool = False,
                  need_initialized: str = "kmeans"):
         super(Codebook, self).__init__()
@@ -134,6 +157,10 @@ class Codebook(nn.Module):
         self.num_codebook_vectors = num_codebook_vectors
         self.embedding = nn.Embedding(num_codebook_vectors, latent_dim)
         self.embedding.weight.data.uniform_(-1.0 / num_codebook_vectors, 1.0 / num_codebook_vectors)
+        self.vq_count = torch.zeros(self.num_codebook_vectors)
+
+        self.update_indices = None  # placeholder
+        self.update_candidates = None  # placeholder
 
         self.normalize = normalize
         if self.use_weighted_sum:
@@ -141,31 +168,76 @@ class Codebook(nn.Module):
         if normalize == "z_trainable":
             self.z_mean = nn.Parameter(torch.zeros(self.latent_dim))
             self.z_log_var = nn.Parameter(torch.zeros(self.latent_dim))
-
+        self.use_restart = use_restart
         self.need_initialized = need_initialized
 
+    @torch.no_grad()
+    def prepare_restart(self, vq_current_count: torch.Tensor, z_flat: torch.Tensor) -> None:
+        """Restart dead entries
+        :param vq_current_count:        (K,)
+        :param z_flat:                  (n, d) = (bhw, d)
+        """
+        n_data = z_flat.shape[0]
+        update_indices = torch.nonzero(vq_current_count == 0, as_tuple=True)[0]
+        n_update = len(update_indices)
+
+        z_indices = list(range(n_data))
+        random.shuffle(z_indices)
+
+        if n_update <= n_data:
+            z_indices = z_indices[:n_update]
+        else:
+            update_indices = update_indices.tolist()
+            random.shuffle(update_indices)
+            update_indices = update_indices[:n_data]
+
+        self.update_indices = update_indices
+        self.update_candidates = z_flat[z_indices]
+
+    @torch.no_grad()
+    def restart(self) -> None:
+        if (self.update_indices is not None) and (self.update_candidates is not None):
+            self.embedding.weight.data[self.update_indices].copy_(self.update_candidates)
+
+            self.vq_count.fill_(0)
+
+            self.update_indices = None
+            self.update_candidates = None
+
     def forward(self, z: torch.Tensor):
+        output = dict()
+
+        self.vq_count = self.vq_count.to(z.device)
+
         z = z.permute(0, 2, 3, 1).contiguous()  # (b, d, h, w) -> (b, h, w, d)
         z_flat = z.view(-1, self.latent_dim)  # (bhw, d)
-
         if self.need_initialized != "none" and self.training:
             if self.need_initialized == "rand":
-                self.prepare_restart(torch.zeros(self.num_codebook, dtype=torch.long, device=z.device),
+                print("Random Initializing ... ")
+                self.prepare_restart(torch.zeros(self.num_codebook_vectors, dtype=torch.long, device=z.device),
                                      z_flat)
                 self.restart()
 
             elif self.need_initialized == "kmeans":
-                clustering = KMeans(init="k-means++", n_clusters=self.num_codebook_vectors, random_state=0)
-                cpu_z_flat = z_flat.detach().cpu().numpy()
-                clustering.fit(cpu_z_flat)
-                centroids = np.array(clustering.cluster_centers_)
-                centroids = torch.from_numpy(centroids).float().to(z.device)
+                print("Kmeans Initializing ... ")
+                # clustering = KMeans(init="k-means++", n_clusters=self.num_codebook_vectors, random_state=0)
+                # cpu_z_flat = z_flat.detach().cpu().numpy()
+                # clustering.fit(cpu_z_flat)
+                # centroids = np.array(clustering.cluster_centers_)
+                # centroids = torch.from_numpy(centroids).float().to(z.device)
+                # self.embedding.weight.data.copy_(centroids)
+                kmeans = faiss.Kmeans(d=z_flat.shape[-1], k=self.num_codebook_vectors, verbose=True, gpu=True)
+                cpu_z_flat = z_flat.detach().cpu().numpy().astype(np.float32)
+                kmeans.train(cpu_z_flat)
+                centroids = torch.from_numpy(kmeans.centroids).float().to(z.device)
                 self.embedding.weight.data.copy_(centroids)
 
             elif self.need_initialized == "uni":
+                print("Uni Initializing ... ")
                 nn.init.xavier_uniform_(self.embedding.weight)
 
             elif self.need_initialized == "normal":
+                print("Normal Initializing ... ")
                 nn.init.xavier_normal_(self.embedding.weight)
             self.need_initialized = "none"
 
@@ -200,11 +272,37 @@ class Codebook(nn.Module):
 
         min_encoding_indices = torch.argmin(d, dim=1)
         distance_prob = F.softmax(-d / 0.5, dim=1)
+        vq_indices = torch.argmin(d, dim=1)
 
         if self.use_weighted_sum:
             z_q = torch.matmul(distance_prob, codebook_norm)  # TODO check temperature scaling
         else:
             z_q = self.embedding(min_encoding_indices)
+
+        # avoid collapse
+        if self.training:
+            with torch.no_grad():
+                vq_indices_one_hot = F.one_hot(vq_indices, self.num_codebook_vectors).to(z.dtype)  # (n, K)
+
+                vq_current_count = torch.sum(vq_indices_one_hot, dim=0)  # (K,)
+                vq_current_count = all_reduce_tensor(vq_current_count, op="sum")
+
+                self.vq_count += vq_current_count
+
+                # vq_current_hist = get_histogram_count(vq_current_count, prefix="current")
+                # vq_hist = get_histogram_count(self.vq_count, prefix="total")
+                # output.update(vq_hist)
+                # output.update(vq_current_hist)
+
+                if self.use_restart:
+                    self.prepare_restart(vq_current_count, z_flat)
+
+                # if self.use_split:
+                #     n_split = self.split(vq_current_count)  # not-used count
+                # else:
+                #     n_split = len(torch.nonzero(vq_current_count == 0, as_tuple=True)[0])
+                output["codebook-usage"] = (self.num_codebook_vectors - len(
+                    torch.nonzero(vq_current_count == 0, as_tuple=True)[0])) / self.num_codebook_vectors  # used ratio
 
         # compute loss for embedding
         codebook_loss = F.mse_loss(z_q, z_norm.detach())  # make codebook to be similar to input
@@ -215,7 +313,6 @@ class Codebook(nn.Module):
             z_q = z_norm + (z_q - z_norm).detach()
         z_q = z_q.view(z.shape).permute(0, 3, 1, 2).contiguous()
 
-        output = dict()
         output["vq-loss"] = q_loss
 
         return z_q, output, distance_prob
@@ -246,7 +343,7 @@ class ProductQuantizerWrapper(nn.Module):
         self.pq_dim = embed_dim // num_pq
 
         self.quantizers = nn.ModuleList([
-            quantizer_cls(num_codebook, self.pq_dim, beta=beta, normalize=normalize,
+            quantizer_cls(num_codebook, self.pq_dim, beta=beta, normalize=normalize, use_restart=use_restart,
                           use_weighted_sum=use_weighted_sum, need_initialized=need_initialized)
             for _ in range(self.num_pq)
         ])

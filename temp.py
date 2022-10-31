e924cbfef04519c9851e633f98d2e199eb679fbd
jiyoungkim <pitlover@naver.com>
2022-10-31 14:42:42 +0900

    [FIX] 10/31 revised dec

--- temp.py
+++ temp.py
@@ -3,6 +3,7 @@ import torch
 corr_matrix = torch.load("./pq0_correlation_matrix/pq_mask_jsd0.1/512/0.01_1.pt")
 # corr_matrix2000 = torch.load("./pq0_correlation_matrix/pq_mask_jsd0.1/512/0.01_2001.pt")
 print(corr_matrix, corr_matrix.shape)
-print(torch.mean(corr_matrix, dim=-1))
+avg_p = torch.mean(corr_matrix, dim=-1)
+print(avg_p, avg_p.shape)
 # print(corr_matrix2000)
 
